3b56a64bc31d5fcb88a3f8926ba62a6f57c6b1a8
ickk <git@ickk.io>
2022-09-05 22:57:55 +0200

    rustfmt and appease https://github.com/rust-lang/rust/issues/41620

--- src/math.rs
+++ src/math.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Sub, Mul, Div, Neg};
+use std::ops::{Add, Div, Mul, Neg, Sub};
 
 type Tuple3 = (f32, f32, f32);
 
@@ -161,7 +161,7 @@ impl Div<f32> for Vector {
 
 impl Vector {
   pub fn magnitude(self) -> f32 {
-    (self.x*self.x + self.y*self.y + self.z*self.z).sqrt()
+    (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
   }
 
   pub fn norm(self) -> Vector {
@@ -169,19 +169,20 @@ impl Vector {
   }
 
   pub fn dot(self, rhs: Vector) -> f32 {
-    self.x*rhs.x + self.y*rhs.y + self.z*rhs.z
+    self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
   }
 
   pub fn cross(self, rhs: Vector) -> Vector {
     Vector {
-      x: self.y*rhs.z - self.z*rhs.y,
-      y: self.z*rhs.x - self.x*rhs.z,
-      z: self.x*rhs.y - self.y*rhs.x,
+      x: self.y * rhs.z - self.z * rhs.y,
+      y: self.z * rhs.x - self.x * rhs.z,
+      z: self.x * rhs.y - self.y * rhs.x,
     }
   }
 }
 
 #[cfg(test)]
+#[allow(unused_variables)]
 mod tests {
   use super::*;
 
@@ -189,7 +190,12 @@ mod tests {
   fn construct_point() {
     // Construct a point from a tuple
     let point: Point = (1.0, 2.0, 3.0).into();
-    assert!(matches!(point, Point {x: 1.0, y: 2.0, z: 3.0}));
+    let expected = Point {
+      x: 1.0,
+      y: 2.0,
+      z: 3.0,
+    };
+    assert!(matches!(point, expected));
   }
 
   #[test]
@@ -198,11 +204,21 @@ mod tests {
     let point: Point = (1.0, 2.0, 3.0).into();
     let vector: Vector = (1.5, 2.5, 3.5).into();
     let sum = point + vector;
-    assert!(matches!(sum, Point {x: 2.5, y: 4.5, z: 6.5}));
+    let expected = Point {
+      x: 2.5,
+      y: 4.5,
+      z: 6.5,
+    };
+    assert!(matches!(sum, expected));
 
     // Add a Point to a Vector
     let sum = vector + point;
-    assert!(matches!(sum, Point {x: 2.5, y: 4.5, z: 6.5}));
+    let expected = Point {
+      x: 2.5,
+      y: 4.5,
+      z: 6.5,
+    };
+    assert!(matches!(sum, expected));
   }
 
   #[test]
@@ -210,26 +226,54 @@ mod tests {
     let point: Point = (1.0, 2.0, 3.0).into();
     let vector: Vector = (1.5, 2.5, 3.5).into();
     let result = point - vector;
-    assert!(matches!(result, Point {x: -0.5, y: -0.5, z: -0.5}));
+    let expected = Point {
+      x: -0.5,
+      y: -0.5,
+      z: -0.5,
+    };
+    assert!(matches!(result, expected));
   }
 
   #[test]
   fn construct_vector() {
     // Construct a Vector from a tuple
     let vector: Vector = (1.0, 2.0, 3.0).into();
-    assert!(matches!(vector, Vector {x: 1.0, y: 2.0, z: 3.0}));
+    let expected = Vector {
+      x: 1.0,
+      y: 2.0,
+      z: 3.0,
+    };
+    assert!(matches!(vector, expected));
 
     // Construct a Vector by subtracting one point from another
-    let point_a = Point { x: 1.0, y: 2.0, z: 3.0 };
-    let point_b = Point { x: 0.1, y: 0.2, z: 0.3 };
+    let point_a = Point {
+      x: 1.0,
+      y: 2.0,
+      z: 3.0,
+    };
+    let point_b = Point {
+      x: 0.1,
+      y: 0.2,
+      z: 0.3,
+    };
     let vector = point_a - point_b;
-    assert!(matches!(vector, Vector {x: 0.9, y: 1.8, z: 2.7}))
+    let expected = Vector {
+      x: 0.9,
+      y: 1.8,
+      z: 2.7,
+    };
+    assert!(matches!(vector, expected))
   }
 
   #[test]
   fn negate_vector() {
     let vector = Vector::from((1.0, 2.0, 3.0));
-    assert!(matches!(-vector, Vector {x: -1.0, y: -2.0, z: -3.0}));
+    let expected = Vector {
+      x: -1.0,
+      y: -2.0,
+      z: -3.0,
+    };
+    assert!(matches!(-vector, expected));
   }
 
   #[test]
@@ -237,7 +281,12 @@ mod tests {
     let vector_a: Vector = (1.0, 2.0, 3.0).into();
     let vector_b: Vector = (0.1, 0.2, 0.3).into();
     let sum = vector_a + vector_b;
-    assert!(matches!(sum, Vector {x: 1.1, y: 2.2, z: 3.3}));
+    let expected = Vector {
+      x: 1.1,
+      y: 2.2,
+      z: 3.3,
+    };
+    assert!(matches!(sum, expected));
   }
 
   #[test]
@@ -245,7 +294,12 @@ mod tests {
     let vector_a: Vector = (1.0, 2.0, 3.0).into();
     let vector_b: Vector = (0.1, 0.2, 0.3).into();
     let result = vector_a - vector_b;
-    assert!(matches!(result, Vector {x: 0.9, y: 1.8, z: 2.7}));
+    let expected = Vector {
+      x: 0.9,
+      y: 1.8,
+      z: 2.7,
+    };
+    assert!(matches!(result, expected));
   }
 
   #[test]
@@ -254,10 +308,20 @@ mod tests {
     let scalar = 2.0;
     let vector: Vector = (0.5, 1.0, 2.0).into();
     let result = scalar * vector;
-    assert!(matches!(result, Vector {x: 1.0, y: 2.0, z: 4.0}));
+    let expected = Vector {
+      x: 1.0,
+      y: 2.0,
+      z: 4.0,
+    };
+    assert!(matches!(result, expected));
     // multiple scalar on the right
     let result = vector * scalar;
-    assert!(matches!(result, Vector {x: 1.0, y: 2.0, z: 4.0}));
+    let expected = Vector {
+      x: 1.0,
+      y: 2.0,
+      z: 4.0,
+    };
+    assert!(matches!(result, expected));
   }
 
   #[test]
@@ -265,7 +329,12 @@ mod tests {
     let scalar = 2.0;
     let vector: Vector = (1.0, 2.0, 4.0).into();
     let result = vector / scalar;
-    assert!(matches!(result, Vector {x: 0.5, y: 1.0, z: 2.0}));
+    let expected = Vector {
+      x: 0.5,
+      y: 1.0,
+      z: 2.0,
+    };
+    assert!(matches!(result, expected));
   }
 
   #[test]
@@ -279,7 +348,11 @@ mod tests {
   fn norm_of_vector() {
     let vector: Vector = (3.0, 4.0, 0.0).into();
     let norm = vector.norm();
-    let expected = Vector {x: 3.0/5.0, y: 4.0/5.0, z: 0.0};
+    let expected = Vector {
+      x: 3.0 / 5.0,
+      y: 4.0 / 5.0,
+      z: 0.0,
+    };
     assert!(matches!(norm, expected));
   }
 
@@ -296,9 +369,19 @@ mod tests {
     let vector_a: Vector = (1.0, 2.0, 3.0).into();
     let vector_b: Vector = (2.0, 3.0, 4.0).into();
     let a_cross_b = vector_a.cross(vector_b);
-    assert!(matches!(a_cross_b, Vector {x: -1.0, y: 2.0, z: -1.0}));
+    let expected = Vector {
+      x: -1.0,
+      y: 2.0,
+      z: -1.0,
+    };
+    assert!(matches!(a_cross_b, expected));
     let b_cross_a = vector_b.cross(vector_a);
-    assert!(matches!(b_cross_a, Vector {x: 1.0, y: -2.0, z: 1.0}));
+    let expected = Vector {
+      x: 1.0,
+      y: -2.0,
+      z: 1.0,
+    };
+    assert!(matches!(b_cross_a, expected));
   }
 }
 
